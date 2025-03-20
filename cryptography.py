e1dffc61b0cac404008229596ae8bbcdf60d5f04
shahnwazakhtar <shahnwazakhtar7292@gmail.com>
2025-03-20 10:52:46 +0000

    commit 3

---
+++ cryptography.py
@@ -0,0 +1,10 @@
+Q.7 = (b) Securing communication
+Q.8 = (b) AES
+Q.9 =  (c) RSA
+Q.10 = (a) Symmetric uses one key; asymmetric uses two keys
+Q.11 = (c) Blowfish
+Q.12 = (d) Weak key attack
+Q.13 = (c) Resistance to Shor’s algorithm
+Q.14 = (b) Using nonces and timestamps
+Q.15 =  (a) PGP
+Q.16 = (a) An encryption method that is immune to quantum computing attacks
